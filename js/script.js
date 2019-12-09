0524362eac718173cd841f652ea96e0a17c7f75d
Adam Goss <adam.goss94@gmail.com>
2019-12-09 15:50:49 +0100

    Add functions for moves, compare and update results

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ function game(){
                      
 
             if(document.getElementById('rounds-number').checkValidity()){
-                const rounds = document.querySelector('input');      
+                const rounds = document.querySelector('input');                
 
                 errorMsg.style.display = 'none';
                 playerScore = 0;
@@ -37,6 +37,102 @@ function game(){
             }
         });
 
-        
-    }    
+        choices();
+    }
+
+    function choices(){
+        const playerChoice = document.querySelectorAll('.btn-choice');
+
+        playerChoice.forEach(function(choice) {
+            choice.addEventListener('click', function(){
+                const rNumber = Math.floor(Math.random()*3);
+                const computerChoices = ['Rock', 'Paper', 'Scissors'];
+                const computerMove = computerChoices[rNumber];
+                const playerMove = this.textContent;                
+
+                if(gamePlaying){
+                    if(roundsLeft > 0){
+
+                        roundsLeft--;
+                        roundsMsg.textContent = roundsLeft;
+                        images[0].src = '/images/' + this.textContent + '.png';
+                        images[1].src = '/images/' + computerMove + '.png';
+
+                        compare(computerMove, playerMove);
+                    }
+                    
+                    if(roundsLeft === 0){
+                        if(playerScore > computerScore){
+                            resultMsg.textContent = 'you won the game!';
+                            return;
+                        }else if(computerScore > playerScore){
+                            resultMsg.textContent = 'you lost the game!';
+                            return;
+                        }else{
+                            resultMsg.textContent = 'the game ended in a draw!'
+                        }
+                    }
+                }
+            });
+        });
+    }
+
+    function compare(computerMove, playerMove){
+    
+        if(computerMove === playerMove){
+            resultMsg.textContent = 'Draw';
+            return;
+        }
+
+        if(playerMove === 'Rock'){
+            if(computerMove === 'Paper'){
+                resultMsg.textContent = 'you lost this round :(';
+                computerScore++;
+                upScore();
+                return;
+            }else{
+                resultMsg.textContent = 'you won this round!';
+                playerScore++;
+                upScore();
+                return;
+            }
+        }
+
+        if(playerMove === 'Paper'){
+            if(computerMove === 'Scissors'){
+                resultMsg.textContent = 'you lost this round :(';
+                computerScore++;
+                upScore();
+                return;
+            }else{
+                resultMsg.textContent = 'you won this round!';
+                playerScore++;
+                upScore();
+                return;
+            }
+        }
+
+        if(playerMove === 'Scissors'){
+            if(computerMove === 'Rock'){
+                resultMsg.textContent = 'you lost this round :(';
+                computerScore++;
+                upScore();
+                return;
+            }else{
+                resultMsg.textContent = 'you won this round!';
+                playerScore++;
+                upScore();
+                return;
+            }
+        }
+    }
+
+    function upScore(){
+        const pScore = document.getElementById('player-score');
+        const cScore = document.getElementById('computer-score');
+
+        pScore.textContent = playerScore;
+        cScore.textContent = computerScore;
+    }
+    
 }
